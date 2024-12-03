27329c419d5c62f02b6e586210cc21bddb45974d
Jack Brooker <jbrooker@cymba.co.uk>
2024-12-03 21:14:36 +0000

    init problem 2 day 2!

--- 2024/day2/problem2.c
+++ 2024/day2/problem2.c
@@ -3,6 +3,32 @@
 #include <stdbool.h>
 #include <string.h>
 
+bool is_valid(const int *numbers_array, const int length) {
+  if (length < 2) {
+    return true;
+  }
+
+  bool valid = true;
+  bool increasing = numbers_array[0] < numbers_array[1];
+
+  for (int i = 1; i <= length; i++) {
+    int prev = numbers_array[i - 1];
+    int curr = numbers_array[i];
+
+    if (abs(curr - prev) > 3) {
+      valid = false;
+    }
+
+    // if (increasing != curr > prev) {
+    //   valid = false;
+    // } else if (curr == prev) {
+    //   valid = false;
+    // }
+  }
+
+  return valid;
+}
+
 int main() {
   FILE *file = fopen("example-input1.txt", "r");
 
@@ -14,62 +40,46 @@ int main() {
   int valid_rows = 0;
   char line[100];
 
+  int *numbers = NULL;
+  size_t counter = 0;
+  size_t capacity = 20;
+  numbers = malloc(capacity * sizeof(int));
+
   while (fgets(line, sizeof(line), file)) {
+    line[strcspn(line, "\n")] = 0;
     printf("line: %s", line);
     char *token = strtok(line, " ");
+    numbers = malloc(capacity * sizeof(int));
 
-    int prev = strtol(token, NULL, 10);
-    int curr = -1;
+    while (token != NULL) {
+      if (counter > capacity) {
+        capacity *= 2;
+        int *temp_numbers = realloc(numbers, capacity * sizeof(int));
 
-    bool line_valid = true;
-    bool skip_used = false;
-    bool last_increased = true;
+        if (temp_numbers == NULL) {
+          perror("Unable to allocate memory");
+          free(numbers);
+          fclose(file);
+          return 1;
+        }
 
-    token = strtok(NULL, " ");
-
-    while (token != NULL && line_valid) {
-      bool token_valid = true;
-      if (curr == -1) {
-        curr = strtol(token, NULL, 10);
-        last_increased = curr > prev;
+        numbers = temp_numbers;
       }
 
-      curr = strtol(token, NULL, 10);
-
-      if (abs(curr - prev) > 3) {
-        token_valid = false;
-      }
-
-      if (last_increased && curr < prev) {
-        token_valid = false;
-      } else if (!last_increased && curr > prev) {
-        token_valid = false;
-      } else if (curr == prev) {
-        token_valid = false;
-      }
-
-      if ((!token_valid && !skip_used) || token_valid) {
-        prev = curr;
-        skip_used = true;
-      } else if (!token_valid && skip_used) {
-        line_valid = false;
-      }
-
-      printf("%i,", curr);
+      numbers[counter] = strtol(token, NULL, 10);;
+      counter += 1;
       token = strtok(NULL, " ");
     }
 
-    if (line_valid) {
-      valid_rows += 1;
-      printf(" // valid\n");
-    } else {
-      printf(" // not valid\n");
-    }
+    bool valid = is_valid(numbers, counter);
+
+    printf(" // valid: %d\n", valid);
  }
 
   printf("Total: %i\n", valid_rows);
 
   fclose(file);
+  free(numbers);
   return 0;
 }
 
